e92480b0ca3f7607a24e367131c0544a06555650
Thiago Lourenço <thiago.lourenco@squadra.com.br>
2021-03-13 18:47:35 -0300

    feat: add App background image

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Card from './components/Card.vue';
 
 export default Vue.extend({
   name: 'App',
-  components: {
-    HelloWorld
-  }
 });
 </script>
 
 <style lang="less">
+* {
+  padding:0;
+  margin:0;
+}
+
+body {
+  width: 100vw;
+  height: 100vh;
+}
+
 #app {
+  display: flex;
+  height: 100%;
+  width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 30px 20px;
+  background-image: url('./assets/family-background.jpg');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: 0 -500px;
 }
 </style>
